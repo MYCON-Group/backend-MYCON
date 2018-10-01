30c1d01999d688a1feb8f1d2a4a88ff57504d7dd
jameschesmer <jameschesmer@gmail.com>
2018-10-01 09:26:13 +0100

    initial setup

--- listen.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const { PORT = require('./config').PORT } = process.env;
-
-app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  else console.log(`server listening on ${PORT}...`);
-});
