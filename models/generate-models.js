9334aea614d8bceac443651ab8d499c9b57903f4
willensoll <william.ensoll@gmail.com>
2018-10-01 15:59:45 +0100

    added patch solution for time being, test working

--- models/generate-models.js
+++ models/generate-models.js
@@ -20,37 +20,16 @@ const generateModel = (table) => {
       })
     },
     updateValue: (params, body) => {
-      console.log(body)
       const id = params;
       const table_id = `${table}_id`;
-      const values = Object.values(body)
-      console.log(values)
-      return values.map(value => {
-        console.log(value)
-        return db.one('UPDATE $(table:name) SET $(body:name) = $(value) WHERE $(table_id:name) = $(id) RETURNING*;', {
-          table, body, value, table_id, id
+      const values = Object.values(body).toString()
+      return db.one('UPDATE $(table:name) \
+                       SET $(body:name) = $(values) \
+                      WHERE $(table_id:name) = $(id) RETURNING*;', {
+          table, body, values, table_id, id
         })
-      })
-        .catch(console.log)
+        .catch((err) => console.log(err, '<<'))
     }
-
-
-    // updateValue: (params, body) => {
-    //   // return new Promise((resolve, reject) => {
-    //   const keys = Object.keys(body)
-    //   console.log(keys, 'keys')
-    //   console.log(table)
-    //   const cs = new pgp.helpers.ColumnSet(keys, { table: table })
-    //   console.log(cs, 'cs')
-    //   const query = pgp.helpers.update(body, cs);
-    //   db.none(query)
-    //     .then(() => {
-    //       resolve()
-    //     })
-    //     .catch(console.log)
-    //   // })
-    // }
-
 
     // selectByParameter: (params, ...cols) => {
     //   if (cols.length === 1) cols = cols[0];
--- spec/App.spec.js
+++ spec/App.spec.js
@@ -45,21 +45,15 @@ describe('/api', () => {
     });
     it('PATCH /event/:event_id', () => {
       const event = {
-        "events_name": "newbirthday"
-        // "events_img": "newimg_url_here",
-        // "events_start": "2nd Jan 2019",
-        // "events_end": "2nd Feb 2019",
-        // "events_description": "a new birthday party",
-        // "events_location": "new party central"
+         "events_img": "newimg_url_here"
       }
       return request
         .patch('/api/event/1')
         .send(event)
-        // .expect(201)
+        .expect(201)
         .then(res => {
-          console.log(res.body.msg)
           expect(res.body).to.have.all.keys('event');
-          expect(res.body.event.events_name).to.equal('newbirthday');
+          expect(res.body.event.events_img).to.equal('newimg_url_here');
           expect(res.body.event).to.be.an('object');
         });
     });
