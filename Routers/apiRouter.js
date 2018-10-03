01f240d1dbc58af6231546ccc4eb081ef6f0e6c4
willensoll <william.ensoll@gmail.com>
2018-10-03 09:50:44 +0100

    added nodemon

--- Routers/apiRouter.js
+++ Routers/apiRouter.js
@@ -1,12 +1,16 @@
 const apiRouter = require('express').Router();
 const eventsRouter = require('./eventsRouter');
 const stallsRouter = require('./stallsRouter');
+const cors = require('cors');
 
 apiRouter.route('/')
 // .get((req, res, next) => {
 //   if (err) next(err);
 //   res.render('../view/pages/api.ejs');
 // });
+apiRouter.use(cors()) 
+apiRouter.options('*', cors())
+
 
 apiRouter.use('/events', eventsRouter);
 apiRouter.use('/stalls', stallsRouter);
