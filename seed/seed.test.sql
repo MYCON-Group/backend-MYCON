d5a980ccf825d32e6ba64fbcc920ae071d45df4d
willensoll <william.ensoll@gmail.com>
2018-10-09 10:59:31 +0100

    updated maps

--- seed/seed.test.sql
+++ seed/seed.test.sql
@@ -55,9 +55,9 @@ CREATE TABLE updates
 INSERT INTO events
     (events_name, events_img, events_start, events_end, events_description, events_location, events_map_height, events_map_width)
 VALUES
-    ('Northcoders Party!', 'http://denverconvention.com/uploads/content/Exhibit_Map.jpg',
+    ('Northcoders Party!', 'https://i.postimg.cc/8CV0ySb0/Map1.png',
         '23 november 2018', '24 november 2019', 'A party for Northcoders', 'The Printworks', 832, 1085),
-    ('Comicon', 'https://static1.squarespace.com/static/57329088f8baf36eb1112c8d/t/595c2461ebbd1a1d0aa6fa6a/1499210854353/',
+    ('Comicon', 'https://i.postimg.cc/WzTH7B0v/map2.png',
         '30 december 2018', '31 december 2018', 'gathering of nerd culture', 'Piccadilly trainstation', 724, 1000);
 
 INSERT INTO stall
