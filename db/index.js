f79e0bece3601db761f0631185c80d76755597ec
kin chan <kinluek@gmail.com>
2018-10-09 08:24:58 +0100

    remove console logs on idex.js

--- db/index.js
+++ db/index.js
@@ -1,18 +1,14 @@
 const initOptions = { promiseLib: Promise };
 const pgp = require('pg-promise')(initOptions);
-const dbConfig = require('../config');
-
-console.log(dbConfig)
-console.log(process.env.POSTGRES_HOST)
-console.log(process.env.POSTGRES_USER)
-console.log(process.env.POSTGRES_PASSWORD)
-console.log(process.env.NODE_ENV)
-const db = process.env.NODE_ENV === 'docker' ? pgp({
+const dbConfig = process.env.NODE_ENV === 'docker' ? ({
   host: process.env.POSTGRES_HOST,
   port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD
-}) : pgp(dbConfig);
+}) : require('../config');
+
+
+const db = pgp(dbConfig);
 
 module.exports = db;
